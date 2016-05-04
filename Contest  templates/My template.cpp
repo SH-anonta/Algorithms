9f64b359b30ab7045164dfcf977509cd0ba0dc46
Anonta <anonta9865@gmail.com>
2016-05-04 20:30:51 +0600

    contest template

--- Contest  templates/My template.cpp
+++ Contest  templates/My template.cpp
@@ -46,8 +46,6 @@ template <typename T>void tobin(T n,char*bin){int pos=1<<((8*sizeof(T)-2));while
 template <typename T>inline T LCM(T x,T y){return((x*y)/__gcd(x,y));}
 int bigMod(int b,int e,int m){if(e==0)return 1;if(!e&1){int temp=bigMod(b,e/2,m)%m;return(temp*temp)%m;}else return((b%m)*(bigMod(b,e-1,m))%m)%m;}
 LL todec(char* bin){LL dec= 0;short i= 0, len= strlen(bin) -1;while(bin[i]){if(bin[i++]=='1'){dec += 1<<len;} len--;}return dec;}
-LL tonum(char* str){LL num= *str++ - '0';while(*str){num= 10*num + (*str++ - '0');}return num;}
-void tostr(LL n,char*st){char*bu=st;while(n>0&&st){*st++='0'+n%10;n/=10;}rary(bu,st);*st='\0';}
 inline LL POW(LL base, int exp){LL p= 1; while(exp-->0){p *= base;} return p;}
 bool ispal(char* str, int len){for(int i= 0; i<len/2; i++){if(str[i]==str[len-i-1]){}else return false;}return true;}
 bool comp(const int& a,const int& b){return a>b;} //reverse sort

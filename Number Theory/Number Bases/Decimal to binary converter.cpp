0215463066614860883f151a3fa36747a059dfd2
Anonta <anonta9865@gmail.com>
2016-05-08 17:03:57 +0600

    number base entry

---
+++ Number Theory/Number Bases/Decimal to binary converter.cpp
@@ -0,0 +1,40 @@
+//Code converts a decimal number into it's binary representation
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+//returns a string object containing the bit string of a number
+template <typename T>
+string tobin(T num){
+    string bin;
+    
+    while(num != 0){
+        if(num&1){ //of num is odd
+             bin = '1' + bin;
+        }
+        else bin = '0' + bin;
+        
+        num /= 2;
+    }
+    
+    return bin;
+}
+
+int main(void)
+{
+    string bin;
+    long long num;
+    
+    while(1){
+        cin>>num;
+        
+        bin = tobin(num);
+        
+        cout<< bin <<endl;
+        
+    }
+    
+    return 0;
+}

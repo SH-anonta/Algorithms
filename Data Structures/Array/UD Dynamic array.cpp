78bd66f598af644f2cb8c1b9402f55334c96df19
Anonta <anonta9865@gmail.com>
2016-05-18 14:55:35 +0600

    dynamic array

--- Data Structures/Array/UD Dynamic array.cpp
+++ Data Structures/Array/UD Dynamic array.cpp
@@ -1,4 +1,3 @@
-//UNDER DEVELOPMENT
 // Dynamic array implementation
 
 #include <iostream>
@@ -6,26 +5,25 @@
 using namespace std;
 
 template <typename T>
-class dynamicArray
+class dArray
 {
 private:
     int dCount;
-    int dCapacity;
     T* data;
     
 public:
     //Constructors
     //Default
-    dynamicArray(){
+    dArray(){
         data = NULL;
     }
     
-    explicit dynamicArray(int sz){
+    explicit dArray(int sz){
         data = new T[sz];
         dCount = sz;
     }
     
-    dynamicArray(int sz, const T& cont){
+    dArray(int sz, const T& cont){
         data = new T[sz];
         dCount = sz;
         for(int i= 0; i<sz; i++){
@@ -33,8 +31,19 @@ public:
         }
     }
     
+    //TODO copy constructor
+    dArray(const dArray& cpy){
+        if(this == &cpy) return;
+        
+        clear();
+        resize(cpy.dCount);   //TODO check for bugs
+        for(int i= 0; i<cpy.dCount; i++){
+            data[i] = cpy[i];
+        }
+    }
+    
     //Destructor
-    ~dynamicArray(){
+    ~dArray(){
         delete[] data;
     }
     
@@ -65,10 +74,42 @@ public:
         return data[index];
     }
     
+    void operator=(dArray& cpy){
+        if(cpy.dCount > dCount){
+            clear();
+            data = new T[cpy.dCount];
+            
+        }   
+        
+        dCount = cpy.dCount;
+        for(int i= 0; i<dCount; i++){
+            data[i] = cpy[i];
+        }
+    }
+    
+    void print(){
+        for(int i= 0; i<dCount; i++){
+            cout<< data[i] <<" ";
+        }
+        cout<<endl;
+    }
+    
 };
 
 int main(void)
 {
+    dArray<int> a(5);
     
+    a[0]= 1;
+    a[1]= 2;
+    a[2]= 3;
+    a[3]= 4;
+    
+    a.print();
+    
+    a.resize(10);
+    
+    a[4]= 23;
+    a.print();
     return 0;
 }

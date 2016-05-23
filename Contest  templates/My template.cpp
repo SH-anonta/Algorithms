b9307c2ca93d3e91054b1ef5ee14663774c058a1
SH-anont <anonta9865@gmail.com>
2016-05-23 10:39:21 +0600

    dekete templete

--- Contest  templates/My template.cpp
+++ Contest  templates/My template.cpp
@@ -25,6 +25,7 @@ short CC_;
 #define DD(x_) cout<<">>>>( "<<++CC_<<" ) "<<#x_<<": "<<x_<<endl;
 #define EXT(st_) cout<<"\n>>>Exicution Time: "<<(double)(clock()-st_)/CLOCKS_PER_SEC<<endl;
 
+<<<<<<< HEAD
 template <typename T>
 class List
 {
@@ -63,6 +64,26 @@ public:
         if(this == &cpy){
             return;
         }
+=======
+//constants
+const int SZ= 1E6;
+const int INF= (1<<29);
+const double EPS= 1E-9;
+const double PI= 2*acos(0.0);
+
+//functions templates
+template <class T>void DA(T* x, int l){for(int i=0; i<l;i++)cout<<"["<<i<<"]>> "<<x[i]<<'\n';}
+template <class T>inline void sary(T* st, T* nd){while(st<nd)cin>>*st++;/*sf("%d", st++);*/}
+template <class T>void tobin(T n,char*bin){int pos= 1<<((int)log2(n));while(pos>=1){if((n & pos)==0)*bin='0';else*bin='1';pos>>=1;bin++;}*bin='\0';}
+template <class T>int strOccur(string&s,T&tgt){int oc= 0, p= s.find(tgt);while(p != string::npos){p = s.find(tgt, p+1);oc++;}return oc;}
+template <class T>inline T LCM(T x,T y){return((x*y)/__gcd(x,y));}
+int bigMod(int b,int e,int m){if(e==0)return 1;if(!e&1){int temp=bigMod(b,e/2,m)%m;return(temp*temp)%m;}else return((b%m)*(bigMod(b,e-1,m))%m)%m;}
+LL todec(char* bin){LL dec= 0;short i= 0, len= strlen(bin) -1;while(bin[i]){if(bin[i++]=='1'){dec += 1<<len;} len--;}return dec;}
+inline LL POW(LL base, int exp){LL p= 1; while(exp-->0){p *= base;} return p;}
+bool ispal(char* str, int len){for(int i= 0; i<len/2; i++){if(str[i]==str[len-i-1]){}else return false;}return true;}
+bool comp(const int& a,const int& b){return a>b;}
+
+>>>>>>> parent of a34727c... template update
 
         Node* i= cpy.head;
         head= tail= new Node(i->data);

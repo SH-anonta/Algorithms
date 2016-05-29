3e6e1801f6d984a603021f3407520047edbd78a4
SH-anont <anonta9865@gmail.com>
2016-05-29 21:19:58 +0600

    Java template entry

---
+++ Contest  templates/Java template.java
@@ -0,0 +1,16 @@
+package jd1;
+
+import java.util.Scanner;
+import java.math.*;
+
+class JD1
+{
+    public static void main(String args[]){
+        JD1 MAIN = new JD1();
+        
+        Scanner sc = new Scanner(System.in);
+//        BigInteger n = new BigInteger("1");
+        
+        System.out.println("ll");
+    }
+}

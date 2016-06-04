ac07c3961c58fc0b13d892be7e22b414c76dc6e5
SH-anont <anonta9865@gmail.com>
2016-06-04 21:56:20 +0600

    Number base entry

---
+++ Number Theory/Number Bases/Binary to decimal (Nested multiplication).cpp
@@ -0,0 +1,43 @@
+//Converts Binary (bit string) to decimal (long long) using nested multiplication
+
+#include <iostream>
+#include <string>
+#include <cstdio>
+#include <cstring>
+
+//converts string object 
+long long toDecimal(const std::string& bin){
+    long long res= bin[0] - '0';
+    for(int i= 1; bin[i] !='\0'; i++){
+        res *= 2;
+        res += bin[i] - '0';
+    }
+    
+    return res;
+}
+
+//converts C string / char array 
+long long toDecimal(const char* bin){
+    long long res= bin[0] - '0';
+    for(int i= 1; bin[i] !='\0'; i++){
+        res *= 2;
+        res += bin[i] - '0';
+    }
+    
+    return res;
+}
+
+int main(void)
+{
+    using std::cout;
+    using std::endl;
+    
+    std::string bin = "11111111110000010110101011010";
+    //Using string object:
+    cout<< "Binary: " << bin<< "\nDecimal: " << toDecimal(bin)  <<endl;
+    //Using c string:
+    cout<< "\nBinary: " << bin<< "\nDecimal: " << toDecimal(bin.c_str())  <<endl;
+    
+    
+    return 0;
+}

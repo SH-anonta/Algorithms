32d24ac9cf10736fecc45b5f7a970e06243afc66
SH-anont <anonta9865@gmail.com>
2016-07-23 14:15:23 +0600

    Added comments to power set generation Backtracking

--- Number Theory/Combinatorial search/Generating power set (Backtracking).cpp
+++ Number Theory/Combinatorial search/Generating power set (Backtracking).cpp
@@ -1,4 +1,21 @@
-Generates power set of a given setarray using recursive Backtracking
+//Generates power set of a given set/array using recursive Backtracking
+//The figure below illustrates how this works:
+//Each branch/edge represents a recursive call
+//and where a, b or c means that element is chosen as part of the subset
+// and _ means no elements were chosen
+//basically weather the a or b or gets pushed into the subset array
+
+//        *START*
+//      /        \
+//     a           _
+//    /  \       /   \
+//   b     _     b     _
+//  / \   / \   / \   /  \
+// c   _  c  _  c  _  c   _
+//abc  ab ac a  bc  b c  [empty set]
+//^^^ following each branch we get all possible subsets
+
+
 #include iostream
 #include vector
 #include string
@@ -6,6 +23,7 @@ Generates power set of a given setarray using recursive Backtracking
 
 template typename T
 void powerSet(T ary, int len, stdvectorT combo){
+    //when there is no more items to choose (or not choose)
     if(len == 0){
         int sz= combo.size();
         for(int i= 0; isz; i++){
@@ -15,12 +33,14 @@ void powerSet(T ary, int len, stdvectorT combo){
         return;
     }
     
+    //dont take the next element
     powerSet(ary, len-1, combo);
     combo.push_back(ary[len-1]);
+    //^take the next element
     powerSet(ary, len-1, combo);
 }
 
-wrapper function
+//wrapper function
 template typename T
 void powerSet(T ary, int len){
     powerSet(ary, len, stdvectorT());

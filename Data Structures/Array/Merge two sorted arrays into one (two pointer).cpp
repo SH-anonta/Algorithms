35d30510f421931e9e965bc1a134c935c4b6a15e
SH-anont <anonta9865@gmail.com>
2016-08-12 18:50:00 +0600

    sorted ary merge update
    increased elegance
    renamed variables

--- Data Structures/Array/Merge two sorted arrays into one (two pointer).cpp
+++ Data Structures/Array/Merge two sorted arrays into one (two pointer).cpp
@@ -2,14 +2,16 @@
 // time complexity: O(n+m) [n and m are length if the two strings]
 #include <iostream>
 
+
+//merges arrays in specified order and returns pointer to new array
 //m and n are lengths of the arrays ary1 and ary2 respectively
 template <typename T>
-T* merge(const T* ary1, int m, const T* ary2, int n){
-    T* combo= new T[m+n];
-    int p1= 0, p2= 0, lim= m+n, i;
+T* merge(const T* ary1, int len1, const T* ary2, int len2){
+    T* combo= new T[len1+len2];
+    int p1= 0, p2= 0, lim= len1+len2, i;
     
     //run until any one of or both array run out of elements
-    for(i= 0; i<lim && p1 < m && p2 < n; i++){
+    for(i= 0; i<lim && p1 < len1 && p2 < len2; i++){
         if(ary1[p1] < ary2[p2]){
             combo[i]= ary1[p1];
             p1++;
@@ -30,20 +32,17 @@ T* merge(const T* ary1, int m, const T* ary2, int n){
     //if an array is shorter than the other
     //insertion stops when all elements of the shorter array are inserted
     //and the remaining elements of the larger array get inserted array
-    if(p1< m){
-        for(; p1<m && i <lim; i++){
-            combo[i]= ary1[p1];
-            p1++;
-        }
+    while(p1<len1){
+        combo[i]= ary1[p1];
+        p1++;
+        i++;
     }
-    if(p2 < n){
-        for(; p2<n && i <lim; i++){
-            combo[i]= ary2[p2];
-            p2++;
-        }
+    while(p2<len2){
+        combo[i]= ary2[p2];
+        p2++;
+        i++;
     }
-    
-    
+
     return combo;
 }
 

f1705c4d7ddfe99a81b9fd2c684a1e6375c93416
Anonta <anonta9865@gmail.com>
2016-10-17 22:58:39 +0600

    pettern matching entry
    Robin karp algorithm

---
+++ String Algorithms/Pattern Matching/Robin karp.cpp
@@ -0,0 +1,49 @@
+//Robin karp algorithm
+//find pattern match in string linear time using rolling hash technique
+#include <iostream>
+
+        
+//find the pattern b in string s
+//return index of first occurrence
+//return -1 if pattern not found
+int robinCarp(const std::string& s, const std::string& b){
+    int n= s.size(), m = b.size();
+    if(n<m) return -1;
+    long long pattern_hash= 0, current_hash= 0, p= 1 , MOD= 7 + 1E15;
+
+    //generate hash of pattern and first m char of string
+    for(int i= m-1; i>= 0; i--){
+        pattern_hash += p*b[i];
+        current_hash += p*s[i];
+        p *= 5;
+    }
+    
+    int pos= 0;    
+    p/= 5;
+    
+    //rolling hash generation
+    while(pos <= n-m){
+        if(pattern_hash == current_hash){
+            if(s.find(b.c_str(), pos, m) != std::string::npos) return pos;
+        }
+                
+        current_hash -= p*s[pos];   //chopping off the left most char's value
+        current_hash *= 5;          //increasing the power of all the comps by 1
+        current_hash += s[pos+m];   //adding a new char value to hash/ 
+        
+        pos++;
+    }
+    
+    return -1;
+}
+
+int main(void)
+{
+    using namespace std;
+    string a= "robin karp";
+    string b= "bin kar";
+    
+    cout<< robinCarp(a, b) <<endl;
+    
+    return 0;
+}

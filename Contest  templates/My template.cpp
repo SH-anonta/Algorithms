d098ed13b176c71abf69c7540fe5444aa935fbce
Anonta <anonta9865@gmail.com>
2016-10-20 10:16:56 +0600

    Template update
    remove un used macros
    change PP macro

--- Contest  templates/My template.cpp
+++ Contest  templates/My template.cpp
@@ -37,12 +37,8 @@ typedef unsigned long long ULL;
 int CC_;
 #define sf scanf
 #define pf printf
-#define PP getchar();
+#define PP cin.get();
 #define NL cout<<endl;
-#define pqueue priority_queue
-#define testb(x_, i_) ((x_&1<<i_)!=0)
-#define setb(x_, i_) (x_=(x_|(1<<i_)))
-#define clrb(x_, i_) (x_=(x_&(!(1<<i_))))
 #define all(container) container.begin(),container.end()
 #define DC(x_) cout<<">>> "<<#x_<<"\n";DA(x_.begin(), x_.end());
 #define DD(x_) cout<<">>>>( "<<++CC_<<" ) "<<#x_<<": "<<x_<<endl;

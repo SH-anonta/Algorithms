11bd321b24a42efa1744194f5671a52ee897d93f
Anonta <anonta9865@gmail.com>
2017-01-10 18:23:35 +0600

    graph algo update
    rewrote  DFS

--- Graph algorithms/Depth first search.cpp
+++ Graph algorithms/Depth first search.cpp
@@ -1,108 +1,93 @@
-//Depth first search
-//traverse the graph in O(n) time
-//the example given bellow is an unweighted undirected graph
-
-/*
- * Sample input:
- * #of nodes 1-n, #of edges m
- * m lines follow with pair of nodes which have edges between them
-
-5 6
-1 3
-2 4
-4 3
-4 5
-2 5
-1 2
- */
-
+//DFS graph traversal
 
 #include <iostream>
 #include <vector>
+#include <cstdio>
 
-//adjacency list for 10K nodes
-std::vector<int> graph[10000];
+std::vector<int> graph[1000];  //the graph is stored here
+char stats[1000];   //w for unvisited, g for exploring b for visited
+int parent[1000];       //keep record of who is who's parent in DFS tree
+int entry_time[1000];    //start time
+int exit_time[1000];    //end   time
 
-//status of current node during traversal(using characters)
-// w-> white: unvisited
-// g-> grey: being processed/adjacent nodes are being visited
-// b-> black: processed/ all adjacent nodes have been visited
-char stat[1000];        
-int entry_time[1000];   //time of visiting a node
-int exit_time[1000];    //time of processed a node
+int visit_time;
 
-int dfs_clock;
-using namespace std;
-//parameters: adjacency list of graph, current position of graph
-void dfs(int current_node){
-    //if this node is already visited
-    if(stat[current_node] != 'w') return;
+void dfs(int n){
+    stats[n]= 'g';
+    entry_time[n]= visit_time;
+    visit_time++;
     
-    int adj_nodes= graph[current_node].size();
+    int len= graph[n].size(), next;
     
-    //flag this node as visited(grey)
-    stat[current_node]= 'g';
-    entry_time[current_node]= ++dfs_clock;
+    for(int i= 0; i<len; i++){
+        next= graph[n][i];
+        if(stats[next] == 'w'){
+            parent[next]= n;
+            dfs(next);
+        }
     
-    //visit this node's adjacent nodes
-    for(int i= 0; i<adj_nodes; i++){
-        dfs(graph[current_node][i]);
     }
     
-    //flag this node as processed (black)
-    stat[current_node]= 'b';
-    exit_time[current_node]= ++dfs_clock;
+    stats[n]= 'b';
+    exit_time[n]= visit_time;
+    visit_time++;
+    
 }
 
 int main(void)
 {
     using namespace std;
+    visit_time= 1;
     
-    //n nodes, m edges
-    int n, m, u, v;
+    int n;  // n nodes
+    int m;  // m edges
+    int u, v; //two terminals an edge
     
-    dfs_clock= 0;
+    //take graph input
     cin>>n>>m;
     
-    //mark all nodes as unvisited(white)
-    for(int i=1; i<= n; i++){
-        stat[i]= 'w';
+    //prepare for graph traversal
+    for(int i= 0; i<=n; i++){
+        graph[i].clear();
+        parent[i]= i; //
+        stats[i]= 'w'; //all unvisited
     }
     
-    //take edges from input
-    while(m--){
+    //take edge input (undirected edges)
+    for(int i= 0; i<m; i++){
         cin>>u>>v;
         
         graph[u].push_back(v);
         graph[v].push_back(u);
     }
     
-    //start DFS from node 1
-    dfs(1);
-    
-    
-    ///////////////////////////////////////
-    //displaying results of DFS
-    cout<< "Node:  ";
-    for(int i=1; i<= n; i++){
-        cout.width(3);
-        cout<< i<< " ";
+    //start graph traversal
+    for(int i= 1; i<=n; i++){
+        if(stats[i] == 'w'){
+            dfs(i);
+        }
     }
     
-    cout<<endl;
-    cout<< "Entry: ";
-    for(int i=1; i<= n; i++){
-        cout.width(3);
-        cout<< entry_time[i]<<" ";
+    //output DFS tree stats
+    cout<< "node:\tparent:\tentry:\texit"<<endl;
+    for(int i= 1; i<= n; i++){
+        cout<< i << "\t"<< parent[i] << "\t"<< entry_time[i] << "\t"<< exit_time[i] <<endl;
     }
-    cout<<endl;
-    cout<< "Exit:  ";
-    for(int i=1; i<= n; i++){
-        cout.width(3);
-        cout<< exit_time[i]<<" ";
-    }
-    cout<<endl;
     
+            
     return 0;
 }
 
+//sample input
+/*
+ 
+6 7
+1 2
+2 5
+1 3
+3 4
+2 3
+5 4
+1 6
+ 
+ */

b04eae1e6bb0e1e4fe51a7c60abf49a203c53e0a
Anonta <SH-anonta@users.noreply.github.com>
2017-07-31 18:34:23 +0600

    CSP Entry 8Queen problem

---
+++ Combinatorial search/Constraint Satisfaction problem/8 Queen Problem.py
@@ -0,0 +1,74 @@
+import sys
+
+
+def readBoard(readable):
+    board = []
+    for r in range(8):
+        board.append(readable.readline())
+
+    return board
+
+
+# hold true for columns and rows that are blocked
+brow = [False] * 8
+bcol = [False] * 8
+queen_positions = []  # stores ordered pairs of x y positions of currently inserted queens
+
+
+def printBoard(board):
+    print('---------------')
+    for r in board:
+        print(' '.join([str(x) for x in r]))
+    print('---------------')
+
+
+def conflicts(x, y):
+    ''' return if placing a queen in x y opsition creates conflicts '''
+    if brow[x] or bcol[y]: return True
+
+    for r, c in queen_positions:
+        if (abs(r - x) == abs(c - y)): return True
+    return False
+
+
+
+def nQueen(g, n):
+    # printBoard(g)
+    # input()
+
+    'try to put n queens on the g board'
+
+    if n == 0:
+        printBoard(g)
+        return
+
+    for r in range(8):
+        for c in range(8):
+            if conflicts(r, c): continue
+
+            brow[r] = True
+            bcol[c] = True
+            queen_positions.append((r, c))
+
+            g[r][c] = 'Q'
+
+            nQueen(g, n - 1)
+
+            g[r][c] = '#'
+            brow[r] = False
+            bcol[c] = False
+            queen_positions.remove((r, c))
+
+
+def main():
+    g = [['#' for y in range(8)] for x in range(8)]
+    nQueen(g, 8)
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
